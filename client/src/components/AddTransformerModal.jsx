570b80903f4f8ee03b40a9e53aed85c4d7297417
Yaseema Rusiru <yaseemarusiru@gmail.com>
2025-08-18 13:39:48 +0530

    Modified Add Transformer window

--- client/src/components/AddTransformerModal.jsx
+++ client/src/components/AddTransformerModal.jsx
@@ -10,7 +10,8 @@ const AddTransformerModal = ({ show, handleClose, onTransformerAdded }) => {
     const [poleId, setPoleId] = useState('');
     const [tType, setType] = useState('');
     const [error, setError] = useState('');
-
+    const transformerTypes = ['Bulk', 'Distribution'];
+    const regions = ['Galle', 'Nugegoda', 'Colombo', 'Kurunegala', 'Gampaha', 'Ampara','Matara'];
     const handleSubmit = async (e) => {
         e.preventDefault();
         setError('');
@@ -42,7 +43,7 @@ const AddTransformerModal = ({ show, handleClose, onTransformerAdded }) => {
     return (
         <Modal show={show} onHide={handleClose}>
             <Modal.Header closeButton>
-                <Modal.Title>Add New Transformer</Modal.Title>
+                <Modal.Title>Add Transformer</Modal.Title>
             </Modal.Header>
             <Modal.Body>
                 {error && <p className="text-danger">{error}</p>}
@@ -51,39 +52,60 @@ const AddTransformerModal = ({ show, handleClose, onTransformerAdded }) => {
                         <Form.Label>Transformer No.</Form.Label>
                         <Form.Control
                             type="text"
-                            placeholder="e.g., AZ-8890"
+                            placeholder="Transformer No"
                             value={transformerId}
                             onChange={(e) => setTransformerId(e.target.value)}
                             required
                         />
                     </Form.Group>
-                    <Form.Group className="mb-3">
-                        <Form.Label>Location</Form.Label>
-                        <Form.Control
-                            type="text"
-                            placeholder="e.g., Nugegoda"
-                            value={location}
-                            onChange={(e) => setRegion(e.target.value)}
-                            required
-                        />
-                    </Form.Group>
+
                     <Form.Group className="mb-3">
                         <Form.Label>Pole No.</Form.Label>
                         <Form.Control
                             type="text"
-                            placeholder="e.g., EN-xxxx"
+                            placeholder="Pole No"
                             value={poleId}
                             onChange={(e) => setPoleId(e.target.value)}
                             required
                         />
                     </Form.Group>
+
                     <Form.Group className="mb-3">
-                        <Form.Label>Type</Form.Label>
-                        <Form.Control
-                            type="number"
-                            placeholder="e.g., 100.0"
+                        <Form.Label>Region</Form.Label>
+                        <Form.Select
+                            type="text"
+                            placeholder="Type"
                             value={tType}
                             onChange={(e) => setType(e.target.value)}
+                            required
+                        >
+                            {regions.map(type => (
+                                <option key={type} value={type}>{type}</option>
+                                ))}
+                        </Form.Select>
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Type</Form.Label>
+                        <Form.Select
+                            type="text"
+                            placeholder="Type"
+                            value={tType}
+                            onChange={(e) => setType(e.target.value)}
+                            required
+                        >
+                            {transformerTypes.map(type => (
+                                <option key={type} value={type}>{type}</option>
+                                ))}
+                        </Form.Select>
+
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Location Details</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Location Details"
+                            value={region}
+                            onChange={(e) => setRegion(e.target.value)}
                             required
                         />
                     </Form.Group>
--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,8 +1,8 @@
 -- Insert 5 sample transformers
 INSERT INTO TRANSFORMER (id, transformer_id, pole_id, region, type) VALUES
-(1, 'AZ-8890', 'EN-122A', 'Nugegoda', 100.0),
-(2, 'AZ-1649', 'EN-123A', 'Nugegoda', 150.0),
-(3, 'AZ-7316', 'EN-123A', 'Maharagama', 100.0),
-(4, 'AZ-4613', 'EN-123B', 'Maharagama', 200.0),
-(5, 'AX-8993', 'EN-122B', 'Kottawa', 150.0);
+(1, 'AZ-8890', 'EN-122A', 'Nugegoda', Bulk),
+(2, 'AZ-1649', 'EN-123A', 'Nugegoda', Bulk),
+(3, 'AZ-7316', 'EN-123A', 'Maharagama', Distribution),
+(4, 'AZ-4613', 'EN-123B', 'Maharagama', Bulk),
+(5, 'AX-8993', 'EN-122B', 'Kottawa', Distribution);
 
